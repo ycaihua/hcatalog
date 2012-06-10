3838aec0dbeaed8608f4bf9a61dd838ddb3199f7
Sushanth Sowmyan <khorgath@apache.org>
2012-06-10 07:09:24 +0000

    HCATALOG-423 HCatalog should include SerDe-reported fields in the table schema (traviscrawford via khorgath)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hcatalog/trunk@1348538 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    	CHANGES.txt
    	src/java/org/apache/hcatalog/data/schema/HCatSchema.java

--- src/java/org/apache/hcatalog/common/HCatUtil.java
+++ src/java/org/apache/hcatalog/common/HCatUtil.java
@@ -188,10 +188,9 @@ public class HCatUtil {
         return client.getTable(dbName, tableName);
     }
 
-    public static HCatSchema getTableSchemaWithPtnCols(Table table)
-            throws IOException {
-        HCatSchema tableSchema = extractSchemaFromStorageDescriptor(table
-                .getSd());
+    public static HCatSchema getTableSchemaWithPtnCols(Table table) throws IOException {
+        HCatSchema tableSchema = new HCatSchema(HCatUtil.getHCatFieldSchemaList(
+                new org.apache.hadoop.hive.ql.metadata.Table(table).getCols()));
 
         if (table.getPartitionKeys().size() != 0) {
 
